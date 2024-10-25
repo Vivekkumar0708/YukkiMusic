5e8ce928406fc9616dc5252213b25d327dbf3395
Vivek kumar <151810083+Vivekkumar0708@users.noreply.github.com>
2024-10-25 12:54:24 +0530

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -20,12 +20,14 @@ commands = {}
 languages_present = {}
 
 
-def get_command(value: str) -> List:
-    return commands["command"][value]
+def get_command(value: str, lang: str = "en") -> Union[str, List[str]]:
+    if lang not in commands:
+        lang = "en"
+    
+    return commands[lang].get(value, value)
 
-
-def command(cmd: str):
-    cmds = " ".join([f"/{c}" for c in get_command(cmd)])
+def command(cmd: str, lang: str = "en"):
+    cmds = " ".join([f"/{c}" for c in get_command(cmd, lang)])
     return cmds
 
 
@@ -33,13 +35,12 @@ def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./strings"):
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        commands[language_name] = yaml.safe_load(
-            open(r"./strings/" + filename, encoding="utf8")
-        )
-
+for filename in os.listdir(r"./strings/cmds/"):
+    if filename.endswith('.yml'):
+        language_code = filename[:-4]
+        file_path = os.path.join(commands_dir, filename)
+        with open(file_path, 'r', encoding='utf8') as file:
+            commands[language_code] = yaml.safe_load(file)
 
 for filename in os.listdir(r"./strings/langs/"):
     if "en" not in languages:
@@ -64,3 +65,9 @@ for filename in os.listdir(r"./strings/langs/"):
             "There is some issue with the language file inside bot. Please report it to the TheTeamvk at @TheTeamvk on Telegram"
         )
         sys.exit()
+
+if not commands:
+    print(
+        "There's a problem loading the command files. Please report it to the TheTeamVivek at @TheTeamVivek on Telegram"
+    )
+    sys.exit()
